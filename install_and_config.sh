d9887fd4caa8034f031f30daebea55821785b836
KunWuLuan <kunwuluan@gmail.com>
2022-09-29 11:31:34 +0800

    install jq

--- install_and_config.sh
+++ install_and_config.sh
@@ -9,6 +9,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "install some plugins"
 brew install cowsay
 
+echo "install jq"
+brew install jq
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
