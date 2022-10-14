d3dff3190893d35851a5ea45ba40aaf822a57823
KunWuLuan <kunwuluan@gmail.com>
2022-10-14 16:19:09 +0800

    add support for vscode plugin

--- install_and_config.sh
+++ install_and_config.sh
@@ -9,17 +9,21 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "install some plugins"
 brew install cowsay
 
+echo "install zsh plugin: vscode"
+git clone https://github.com/valentinocossar/vscode.git $ZSH_CUSTOM/plugins/vscode
+
 echo "install jq"
 brew install jq
 
+echo "install zsh plugin: zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+echo "install zsh plugin: zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+echo "install fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
 echo "enable some plugins"
-sed -i .back 's/plugins=(git)/plugins=(fzf git cp zsh-syntax-highlighting zsh-autosuggestions rand-quote extract sudo z)/g' ~/.zshrc 
+sed -i .back 's/plugins=(git)/plugins=(fzf git cp zsh-syntax-highlighting zsh-autosuggestions rand-quote extract sudo vscode z)/g' ~/.zshrc 
 
 echo "some common aliases"
 cat >>~/.zshrc <<EOF
